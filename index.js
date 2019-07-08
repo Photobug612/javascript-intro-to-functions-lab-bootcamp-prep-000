6624ccd63fc3db4192cbd638190843aed466e863
Arlene George <aemg@live.com>
2019-07-08 00:08:30 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,41 @@
+function shout(string) {
+  return string.toUpperCase () 
+}
+
+function whisper(string) {
+  return string.toLowerCase ()
+}
+
+function logShout(string) {
+  console.log('HELLO')
+}
+
+function logWhisper(string) {
+  console.log('hello')
+}
+
+function string(string) {
+  
+}
+
+var uppercase = "HELLO"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false
+
+function sayHiToGrandma(string) {
+    if((string) === lowercase) 
+    return "I can\'t hear you!"
+    else if ((string) === uppercase) 
+    return ("YES INDEED!") 
+    else if ((string) === ('I love you, Grandma.'))
+    return ("I love you, too.") }
