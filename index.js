7494c258e99475d10b8fc76dfecbea33244477a5
Arlene George <aemg@live.com>
2019-07-08 00:34:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,9 +33,9 @@ mixedCase.toLowerCase() === mixedCase // false
 mixedCase.toUpperCase() === mixedCase // false
 
 function sayHiToGrandma(string) {
-    if ((string) === lowercase)
+    if ("" === lowercase)
     return 'I can\t hear you!'
-     else if ((string) === uppercase) 
+     else if ("" === uppercase) 
     return ("YES INDEED!") 
     else if ((string) === 'I love you, Grandma.')
     return ("I love you, too.") }
