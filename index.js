6a09e4b65c2835f4eea9075ad09f09e0cabee97a
Arlene George <aemg@live.com>
2019-07-08 00:46:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,5 +40,7 @@ function sayHiToGrandma(string) {
   return "I can\'t hear you!"
   else if (a.toUpperCase() === a)
   return "YES INDEED!"
+  else if ((string) === "I love you, Grandma.")
+  return "I love you, too."
   
 }
